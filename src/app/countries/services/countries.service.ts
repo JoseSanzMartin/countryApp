20b25fee582e8e34c8e6fa60afb386ec7d74a8bb
JoseSanzMartin <josesanzmartin@hotmail.es>
2024-11-16 00:29:51 +0100

    html and ts involving in country and region search cases

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -16,10 +16,23 @@ export class CountriesService {
     const url = `${this.apiUrl}/capital/${term}`;
     return this.http.get<Country[]>( url )
       .pipe(
-        catchError( error => {
-          console.log(error);
-          return of([])
-        })
+        catchError( () => of([]))
+      );
+  }
+
+  searchCountry(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`;
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]))
+      );
+  }
+
+  searchRegion(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${term}`;
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]))
       );
   }
 
