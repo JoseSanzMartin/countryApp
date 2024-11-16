8a159a6682d5217cf65d2d09b2bc7711f2f8cec7
JoseSanzMartin <josesanzmartin@hotmail.es>
2024-11-16 01:10:16 +0100

    Creation of function that show the searched country

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { of, Observable, catchError } from 'rxjs';
+import { of, Observable, catchError, map } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({
@@ -11,6 +11,15 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) { }
+
+  searchCountryByAlphaCode( code: string): Observable<Country | null> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>( url )
+      .pipe(
+        map ( countries => countries.length > 0 ? countries[0]: null),
+        catchError( () => of(null))
+      );
+  }
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
