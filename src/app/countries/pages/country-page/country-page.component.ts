56d6a05632ce50f63285042836fde0680aecc1e2
JoseSanzMartin <josesanzmartin@hotmail.es>
2024-11-18 19:28:43 +0100

    Adding info of contry on country page

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -10,6 +11,8 @@ import { switchMap } from 'rxjs';
   ]
 })
 export class CountryPageComponent implements OnInit{
+
+  public country?: Country;
 
   constructor ( 
     private activatedRoute: ActivatedRoute,
@@ -27,8 +30,7 @@ export class CountryPageComponent implements OnInit{
           return this.router.navigateByUrl('');
         }
 
-        console.log("Tenemos un país");
-        return
+        return this.country = country;  
       });
   }
 }
